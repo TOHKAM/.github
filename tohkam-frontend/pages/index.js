f9a45d9404e1fa8de9b210a1623fd9524a0c3ffa
amirtechnoide <amirzoutor@gmail.com>
2022-08-28 04:36:20 +0100

    initialise the repo

--- tohkam-frontend/pages/index.js
+++ tohkam-frontend/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../assets/styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome amir <a href="https://nextjs.org">Next.jsdasdasda!</a>
         </h1>
 
         <p className={styles.description}>
@@ -21,7 +21,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
+        <div className={styles.grid}> 
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
